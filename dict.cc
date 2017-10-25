53f9d76c1875470a5c45f98579f053169b71a9f0
maaku33 <mareksmolarczyk33@gmail.com>
2017-10-25 14:12:32 +0200

    Some dictionary handling functions

--- dict.cc
+++ dict.cc
@@ -2,6 +2,7 @@
 #include <map>
 #include <unordered_map>
 #include <vector>
+#include <string>
 #include "dict.h"
 
 typedef std::unordered_map<std::string, std::string> Dict;
@@ -15,4 +16,51 @@ IdentificatorType dict_new() {
     dictionaries.insert(std::pair<IdentificatorType, Dict>(++dictCounter, Dict()));
 
     return dictCounter;
+}
+
+void dict_remove(IdentificatorType id, const char* key) {
+    // maybe special version for dictglobal
+
+    auto dictionaryIt = dictionaries.find(id);
+    if (dictionaryIt != dictionaries.end()) {
+        dictionaryIt->second.erase(key);
+    }
+}
+
+const char* dict_find(IdentificatorType id, const char* key) {
+    auto dictionaryIt = dictionaries.find(id);
+    if (dictionaryIt != dictionaries.end()) {
+        auto stringIt = dictionaryIt->second.find(key);
+        if (stringIt != dictionaryIt->second.end()) {
+            return stringIt->second.c_str();
+        }
+    }
+    
+    /*
+    if (there exists value under key in global dictionary) {
+        return that value
+    }
+    */
+
+    return NULL;
+}
+
+void dict_clear(IdentificatorType id) {
+    // maybe special version for dictglobal
+
+    auto dictionaryIt = dictionaries.find(id);
+    if (dictionaryIt != dictionaries.end()) {
+        dictionaryIt->second.clear();
+    }
+}
+
+void dict_copy(IdentificatorType src_id, IdentificatorType dst_id) {
+    auto srcDictionaryIt = dictionaries.find(src_id);
+    auto dstDictionaryIt = dictionaries.find(dst_id);
+    if (srcDictionaryIt != dictionaries.end() &&
+        dstDictionaryIt != dictionaries.end()) {
+        // do not copy if destination dictionary is dictglobal and amount of keys exceeds size
+        
+        // copy contents
+    }
 }
