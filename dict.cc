2463acf657ac492e01bcfbb84031a572f48e8d0e
maaku33 <mareksmolarczyk33@gmail.com>
2017-10-25 13:03:51 +0200

    Nazwy typedef'ów i ich wykorzystanie

--- dict.cc
+++ dict.cc
@@ -4,15 +4,15 @@
 #include <vector>
 #include "dict.h"
 
-typedef std::unordered_map<std::string, std::string> dict;
-typedef unsigned long identificatorType;
+typedef std::unordered_map<std::string, std::string> Dict;
+typedef unsigned long IdentificatorType;
 
 // consider map<unsigned long, unordered_map>
-std::map<identificatorType, dict> dictionaries;
-unsigned dictCounter = 0;
+std::map<IdentificatorType, Dict> dictionaries;
+IdentificatorType dictCounter = 0;
 
-identificatorType dict_new() {
-    dictionaries.insert(std::pair<identificatorType, dict>(++dictCounter, dict()));
+IdentificatorType dict_new() {
+    dictionaries.insert(std::pair<IdentificatorType, Dict>(++dictCounter, Dict()));
 
     return dictCounter;
 }
