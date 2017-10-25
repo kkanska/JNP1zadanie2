29a8cb8d5dac3b1dbc2f077fff9d8e169af60e57
maaku33 <mareksmolarczyk33@gmail.com>
2017-10-25 17:13:07 +0200

    Added const value and method definition

--- dictglobal.cc
+++ dictglobal.cc
@@ -1,1 +1,9 @@
 #include "dictglobal.h"
+
+#define DICT_GLOBAL_ID 0
+
+const unsigned MAX_GLOBAL_DICT_SIZE = 42;
+
+unsigned long dict_global() {
+    return DICT_GLOBAL_ID;
+}
--- dictglobal.h
+++ dictglobal.h
@@ -1,4 +1,8 @@
 #ifndef __DICTGLOBAL_H__
 #define __DICTGLOBAL_H__
 
+extern const unsigned MAX_GLOBAL_DICT_SIZE;
+
+unsigned long dict_global();
+
 #endif /* __DICTGLOBAL_H__ */
