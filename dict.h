74770b447a4acba508b9e48e87c6b1052d75c542
maaku33 <mareksmolarczyk33@gmail.com>
2017-10-26 18:24:06 +0200

    Added method definitions

--- dict.h
+++ dict.h
@@ -1,4 +1,14 @@
 #ifndef __DICT_H__
 #define __DICT_H__
 
+unsigned long dict_new();
+
+void dict_remove(unsigned long id, const char* key);
+
+const char* dict_find(unsigned long id, const char* key);
+
+void dict_clear(unsigned long id);
+
+void dict_copy(unsigned long src_id, unsigned long dst_id);
+
 #endif /* __DICT_H__ */
