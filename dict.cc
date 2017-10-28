efac8de3c661da9e7c3628a393d5b8bf0abaeb1c
maaku33 <mareksmolarczyk33@gmail.com>
2017-10-28 10:52:16 +0200

    Added message function calls in already existing methods

--- dict.cc
+++ dict.cc
@@ -14,54 +14,6 @@ typedef unsigned long IdentificatorType;
 std::map<IdentificatorType, Dict> dictionaries;
 IdentificatorType dictCounter = dict_global();
 
-IdentificatorType dict_new() {
-    dictionaries.insert(std::pair<IdentificatorType, Dict>(++dictCounter, Dict()));
-
-    return dictCounter;
-}
-
-void dict_remove(IdentificatorType id, const char* key) {
-    auto dictionaryIt = dictionaries.find(id);
-    if (dictionaryIt != dictionaries.end()) {
-        dictionaryIt->second.erase(key);
-    }
-}
-
-const char* dict_find(IdentificatorType id, const char* key) {
-    auto dictionaryIt = dictionaries.find(id);
-    if (dictionaryIt != dictionaries.end()) {
-        auto stringIt = dictionaryIt->second.find(key);
-        if (stringIt != dictionaryIt->second.end()) {
-            return stringIt->second.c_str();
-        }
-    }
-    
-    auto stringIt = dictionaries.at(0).find(key);
-    if (stringIt != dictionaries.at(0).end()) {
-        return stringIt->second.c_str();
-    }
-
-    return NULL;
-}
-
-void dict_clear(IdentificatorType id) {
-    auto dictionaryIt = dictionaries.find(id);
-    if (dictionaryIt != dictionaries.end()) {
-        dictionaryIt->second.clear();
-    }
-}
-
-void dict_copy(IdentificatorType src_id, IdentificatorType dst_id) {
-    auto srcDictionaryIt = dictionaries.find(src_id);
-    auto dstDictionaryIt = dictionaries.find(dst_id);
-    if (srcDictionaryIt != dictionaries.end() &&
-        dstDictionaryIt != dictionaries.end()) {
-        // do not copy if destination dictionary is dictglobal and amount of keys exceeds size
-        
-        // copy contents
-    }
-}
-
 static void dict_not_found_msg(std::string funcName, IdentificatorType id) {
     std::cerr << funcName << ": dict " << id << " does not exist" << "\n";
 }
@@ -96,10 +48,86 @@ static void dict_copied_msg(std::string funcName,
               << " has been copied into dict " << dst_id << "\n";
 }
 
-static void searching_global_dictionary_msg(std::string funcName) {
+static void search_global_dict_msg(std::string funcName) {
     std::cerr << funcName << ": looking up the Global Dictionary\n";
 }
 
-static void clear_msg(std::string funcName, IdentificatorType id) {
+static void dict_cleared_msg(std::string funcName, IdentificatorType id) {
     std::cerr << funcName << ": dict " << id << " has been cleared\n";
+}
+
+IdentificatorType dict_new() {
+    dictionaries.insert(std::pair<IdentificatorType, Dict>(++dictCounter, Dict()));
+
+    return dictCounter;
+}
+
+void dict_remove(IdentificatorType id, const char* key) {
+    auto dictionaryIt = dictionaries.find(id);
+    if (dictionaryIt != dictionaries.end()) {
+        if (dictionaryIt->second.erase(key) > 0) {
+            key_removed_msg("dict_remove", id, key);
+        }
+        else {
+            key_not_found_msg("dict_remove", id, key);
+        }
+    } 
+    else {
+        dict_not_found_msg("dict_remove", id);
+    }
+}
+
+const char* dict_find(IdentificatorType id, const char* key) {
+    auto dictionaryIt = dictionaries.find(id);
+    if (dictionaryIt != dictionaries.end()) {
+        auto stringIt = dictionaryIt->second.find(key);
+        if (stringIt != dictionaryIt->second.end()) {
+            value_found_msg("dict_find", id, key, stringIt->second);
+
+            return stringIt->second.c_str();
+        }
+        else {
+            key_not_found_msg("dict_find", id, key);
+        }
+    }
+    else {
+        dict_not_found_msg("dict_find", id);
+    }
+    
+    search_global_dict_msg("dict_find");
+    
+    auto stringIt = dictionaries.at(dict_global()).find(key);
+    if (stringIt != dictionaries.at(dict_global()).end()) {
+        value_found_msg("dict_find", dict_global(), key, stringIt->second);
+
+        return stringIt->second.c_str();
+    }
+    else {
+        key_not_found_msg("dict_find", dict_global(), key);
+    }
+
+    return NULL;
+}
+
+void dict_clear(IdentificatorType id) {
+    auto dictionaryIt = dictionaries.find(id);
+    if (dictionaryIt != dictionaries.end()) {
+        dict_cleared_msg("dict_clear", id);
+
+        dictionaryIt->second.clear();
+    }
+    else {
+        dict_not_found_msg("dict_clear", id);
+    }
+}
+
+void dict_copy(IdentificatorType src_id, IdentificatorType dst_id) {
+    auto srcDictionaryIt = dictionaries.find(src_id);
+    auto dstDictionaryIt = dictionaries.find(dst_id);
+    if (srcDictionaryIt != dictionaries.end() &&
+        dstDictionaryIt != dictionaries.end()) {
+        // do not copy if destination dictionary is dictglobal and amount of keys exceeds size
+        
+        // copy contents
+    }
 }
