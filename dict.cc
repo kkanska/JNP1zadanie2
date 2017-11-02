74f841955729314ea342aabae67b51a6024027b9
maaku33 <mareksmolarczyk33@gmail.com>
2017-11-02 18:41:57 +0100

    Added checking for nullptr in dict_remove

--- dict.cc
+++ dict.cc
@@ -270,6 +270,11 @@ void dict_insert(unsigned long id, const char* key, const char* value) {
 }
 
 void dict_remove(IdentifierType id, const char* key) {
+    // TODO: add error message
+    if (key == NULL) {
+        return;
+    }
+
     const auto dictionaryIt = dicts().find(id);
 
     if (dictionaryIt != dicts().end()) {
@@ -284,6 +289,7 @@ void dict_remove(IdentifierType id, const char* key) {
 }
 
 const char* dict_find(IdentifierType id, const char* key) {
+    // TODO: add error message
     if (key == NULL) {
         return NULL;
     }
